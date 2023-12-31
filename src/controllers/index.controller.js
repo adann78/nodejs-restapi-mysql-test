c6353b4207c90f46a79ed03105acb84580857180
Adann78 <adanher78@gmail.com>
2023-12-31 08:28:19 -0700

    five commit

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,6 @@
 import {pool} from '../db.js'
-
+import express from 'express'
+const app=express()
 export const ping=async(req,res)=>{
     const [result]=await pool.query('SELECT "pong" AS result')
     res.json(result[0])
